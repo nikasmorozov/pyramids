56831ab180254fb6189954ecd9c06329113ffb6a
nikasmorozov <solarisocean@gmail.com>
2019-11-06 18:38:28 +0200

    sudas

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
 
 let elementsArray = document.querySelectorAll('button');
 let place = document.getElementById("place");
-let symbol = '&#9786;';
+let symbol = '&#128169;';
 let lenght = 25;
 
-elementsArray.forEach(function(elem) {
-    elem.addEventListener("click", function(e) {
-        switch(e.target.textContent) {
+elementsArray.forEach(function (elem) {
+  elem.addEventListener("click", function (e) {
+    switch (e.target.textContent) {
 
-            case "triangle":
-            
-let star = '';
-            
-for (i = 1; i < (lenght + 1); i++) {
-  for (j = 0; j < i; j++) {
-  star += symbol + '&nbsp;';
-};
-  star += '<br>';
-};
-         
-place.innerHTML = star;
-             break;
-             case "pyramid":
-            
-let star2 = '';
-            
-for (i = 1; i < (lenght + 1); i++) {
-  for (j = lenght; j > i; j--) {
-  star2 += '&nbsp;';
-  };
-  for (k = 0; k < i; k++) {
-  star2 += symbol + '&nbsp;' ;
-  };
-  star2 += '<br>';
-};
+      case "triangle":
 
-place.innerHTML = star2;
-             break;
-            case "reversed-pyramid":
-            
-let star3 = '';
-            
-for (i = 0; i < lenght; i++) {
-  for (k = 0; k < i; k++) {
-  star3 += '&nbsp;' ;
-  };
-  for (j = lenght; j > i; j--) {
-  star3 += symbol + '&nbsp;';
-  };
-  star3 += '<br>';
-};
+        let star = '';
 
-place.innerHTML = star3;
-             break;
-          }
-    });
+        for (i = 1; i < (lenght + 1); i++) {
+          for (j = 0; j < i; j++) {
+            star += symbol + '&nbsp;';
+          };
+          star += '<br>';
+        };
+
+        place.innerHTML = star;
+        break;
+      case "pyramid":
+
+        let star2 = '';
+
+        for (i = 1; i < (lenght + 1); i++) {
+          for (j = lenght; j > i; j--) {
+            star2 += '&nbsp;';
+          };
+          for (k = 0; k < i; k++) {
+            star2 += symbol + '&nbsp;';
+          };
+          star2 += '<br>';
+        };
+
+        place.innerHTML = star2;
+        break;
+      case "reversed-pyramid":
+
+        let star3 = '';
+
+        for (i = 0; i < lenght; i++) {
+          for (k = 0; k < i; k++) {
+            star3 += '&nbsp;';
+          };
+          for (j = lenght; j > i; j--) {
+            star3 += symbol + '&nbsp;';
+          };
+          star3 += '<br>';
+        };
+
+        place.innerHTML = star3;
+        break;
+    }
+  });
 });
 
-document.getElementById('clear').addEventListener('click', function(){
+document.getElementById('clear').addEventListener('click', function () {
   place.innerHTML = '';
 }
 )
